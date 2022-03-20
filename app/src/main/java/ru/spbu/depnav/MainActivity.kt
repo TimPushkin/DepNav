43f0665864deea46595b981fdb2b885b3bf67d7a
TimPushkin <pushkin.td@gmail.com>
2022-03-20 19:21:04 +0300

    Extract composable UI from MainActivity

--- app/src/main/java/ru/spbu/depnav/MainActivity.kt
+++ app/src/main/java/ru/spbu/depnav/MainActivity.kt
@@ -3,23 +3,14 @@ package ru.spbu.depnav
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.room.Room
 import kotlinx.coroutines.runBlocking
-import ovh.plrapps.mapcompose.ui.MapUI
 import ru.spbu.depnav.controller.MapFloorSwitcher
 import ru.spbu.depnav.db.AppDatabase
 import ru.spbu.depnav.provider.MarkerProvider
 import ru.spbu.depnav.ui.theme.DepNavTheme
 import ru.spbu.depnav.provider.TileProviderFactory
-import ru.spbu.depnav.ui.FloorSwitch
-import ru.spbu.depnav.ui.SearchField
+import ru.spbu.depnav.ui.MainScreen
 import ru.spbu.depnav.viewmodel.MapViewModel
 
 private const val TAG = "MainActivity"
@@ -54,34 +45,12 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             DepNavTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Box {
-                        MapUI(
-                            modifier = Modifier.fillMaxSize(),
-                            state = mMapViewModel.state
-                        )
-
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            SearchField {
-                                mMarkerProvider.getMarker(it)?.run {
-                                    mMapViewModel.centerOnMarker(idStr)
-                                }
-                            }
-
-                            FloorSwitch(
-                                modifier = Modifier.align(Alignment.End),
-                                onClick = mMapFloorSwitcher::setFloor,
-                                maxFloor = mapInfo.floorsNum
-                            )
-                        }
-                    }
-                }
+                MainScreen(
+                    mapViewModel = mMapViewModel,
+                    markerProvider = mMarkerProvider,
+                    floorsNum = mapInfo.floorsNum,
+                    onFloorSwitch = mMapFloorSwitcher::setFloor
+                )
             }
         }
     }
---
+++ app/src/main/java/ru/spbu/depnav/ui/MainScreen.kt
@@ -0,0 +1,49 @@
+package ru.spbu.depnav.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import ovh.plrapps.mapcompose.ui.MapUI
+import ru.spbu.depnav.provider.MarkerProvider
+import ru.spbu.depnav.viewmodel.MapViewModel
+
+@Composable
+fun MainScreen(
+    mapViewModel: MapViewModel,
+    markerProvider: MarkerProvider,
+    floorsNum: Int,
+    onFloorSwitch: (Int) -> Unit,
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Box {
+            MapUI(
+                modifier = Modifier.fillMaxSize(),
+                state = mapViewModel.state
+            )
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                SearchField {
+                    markerProvider.getMarker(it)?.run { mapViewModel.centerOnMarker(idStr) }
+                }
+
+                FloorSwitch(
+                    modifier = Modifier.align(Alignment.End),
+                    onClick = onFloorSwitch,
+                    maxFloor = floorsNum
+                )
+            }
+        }
+    }
+}
