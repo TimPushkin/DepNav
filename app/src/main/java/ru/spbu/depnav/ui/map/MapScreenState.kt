24c77b3bded7ba97197d19c23caf5b764c0da49e
TimPushkin <pushkin.td@gmail.com>
2022-07-10 16:46:41 +0300

    Link markers' transparency to map scale

--- app/src/main/java/ru/spbu/depnav/ui/map/MapScreenState.kt
+++ app/src/main/java/ru/spbu/depnav/ui/map/MapScreenState.kt
@@ -4,21 +4,30 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import ovh.plrapps.mapcompose.api.ExperimentalClusteringApi
 import ovh.plrapps.mapcompose.api.addLayer
 import ovh.plrapps.mapcompose.api.addLazyLoader
 import ovh.plrapps.mapcompose.api.addMarker
 import ovh.plrapps.mapcompose.api.centerOnMarker
+import ovh.plrapps.mapcompose.api.maxScale
+import ovh.plrapps.mapcompose.api.minScaleSnapshotFlow
 import ovh.plrapps.mapcompose.api.onMarkerClick
 import ovh.plrapps.mapcompose.api.onTap
 import ovh.plrapps.mapcompose.api.removeAllLayers
 import ovh.plrapps.mapcompose.api.removeAllMarkers
 import ovh.plrapps.mapcompose.api.removeMarker
+import ovh.plrapps.mapcompose.api.scale
 import ovh.plrapps.mapcompose.api.setScrollOffsetRatio
 import ovh.plrapps.mapcompose.core.TileStreamProvider
 import ovh.plrapps.mapcompose.ui.state.MapState
@@ -29,6 +38,9 @@ import ru.spbu.depnav.model.MarkerText
 private const val TAG = "MapViewModel"
 
 private const val LAZY_LOADER_ID = "main"
+private const val HIGHLIGHTED_MARKER_ID = -1 // Real IDs start from 1
+private const val MIN_MARKER_VISIBILITY_SCALE = 0.2f
+private const val MAX_MARKER_VISIBILITY_SCALE = 0.5f
 
 /**
  * State of the [MapScreen].
@@ -74,11 +86,19 @@ class MapScreenState : ViewModel() {
 
     private val clickableMarkers = mutableMapOf<String, Pair<Marker, MarkerText>>()
 
+    private var minScaleCollectionJob: Job? = null
+    private var minMarkerVisScale = 0f
+    private val maxMarkerVisScale = state.maxScale.coerceAtMost(MAX_MARKER_VISIBILITY_SCALE)
+    private val markerAlpha
+        get() = (state.scale - minMarkerVisScale) / (maxMarkerVisScale - minMarkerVisScale)
+
     /**
      * Sets the parameters of the displayed map.
      */
     fun setParams(levelsNum: Int, width: Int, height: Int, tileSize: Int) {
+        minScaleCollectionJob?.cancel("State changed")
         state.shutdown()
+
         state = MapState(levelsNum, width, height, tileSize) { scale(0f) }.apply {
             setScrollOffsetRatio(0.5f, 0.5f)
             addLazyLoader(LAZY_LOADER_ID, padding = 20.dp)
@@ -100,6 +120,10 @@ class MapScreenState : ViewModel() {
                 } ?: Log.e(TAG, "Marker $id is not clickable")
             }
         }
+
+        minScaleCollectionJob = state.minScaleSnapshotFlow()
+            .onEach { minMarkerVisScale = it.coerceAtLeast(MIN_MARKER_VISIBILITY_SCALE) }
+            .launchIn(viewModelScope)
     }
 
     private fun placeMarker(marker: Marker, markerText: MarkerText, isHighlighted: Boolean) {
@@ -127,13 +151,19 @@ class MapScreenState : ViewModel() {
                 title = markerText.title ?: "",
                 type = marker.type,
                 isClosed = marker.isClosed,
-                isHighlighted = isHighlighted
+                isHighlighted = isHighlighted,
+                modifier =
+                if (!isHighlighted) {
+                    Modifier.graphicsLayer(alpha = markerAlpha)
+                } else {
+                    Modifier
+                }
             )
         }
     }
 
     private fun highlightMarker(marker: Marker, markerText: MarkerText) {
-        val newMarker = marker.copy(id = Int.MIN_VALUE) // Real IDs start from 1
+        val newMarker = marker.copy(id = HIGHLIGHTED_MARKER_ID)
         val newMarkerText = markerText.copy(markerId = newMarker.id)
 
         highlightedMarker?.let { (oldMarker, _) ->
@@ -141,8 +171,8 @@ class MapScreenState : ViewModel() {
             state.removeMarker(oldMarker.idStr)
         }
 
-        showUI = true
         highlightedMarker = newMarker to newMarkerText
+        showUI = true
         highlightMarker = true
 
         placeMarker(newMarker, newMarkerText, isHighlighted = true)
