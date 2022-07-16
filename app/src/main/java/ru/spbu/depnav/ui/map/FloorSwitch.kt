e6e236f54d54988e088c4a3de276ed58900ac4d0
TimPushkin <pushkin.td@gmail.com>
2022-07-16 20:46:26 +0300

    Add floor switching animation

--- app/src/main/java/ru/spbu/depnav/ui/map/FloorSwitch.kt
+++ app/src/main/java/ru/spbu/depnav/ui/map/FloorSwitch.kt
@@ -1,12 +1,23 @@
 package ru.spbu.depnav.ui.map
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.KeyboardArrowUp
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,6 +27,7 @@ import ru.spbu.depnav.ui.theme.DepNavTheme
 /**
  * Two buttons to switch the current map one floor up or down.
  */
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun FloorSwitch(
     floor: Int,
@@ -37,7 +49,20 @@ fun FloorSwitch(
                 Icon(Icons.Rounded.KeyboardArrowUp, contentDescription = "Up arrow")
             }
 
-            Text(floor.toString(10))
+            AnimatedContent(
+                targetState = floor,
+                transitionSpec = {
+                    if (targetState > initialState) {
+                        slideInVertically { height -> -height } + fadeIn() with
+                                slideOutVertically { height -> height } + fadeOut()
+                    } else {
+                        slideInVertically { height -> height } + fadeIn() with
+                                slideOutVertically { height -> -height } + fadeOut()
+                    } using SizeTransform(clip = false)
+                }
+            ) { targetFloor ->
+                Text(targetFloor.toString(10))
+            }
 
             IconButton(
                 onClick = { onClick(floor - 1) },
