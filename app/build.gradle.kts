6401a424e3d43ad46260cb0c1c935de0781bdf93
Timofei Pushkin <pushkin.td@gmail.com>
2024-08-29 17:29:49 +0200

    Bump app version to `1.4.2`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 object Version {
     private const val MAJOR = 1
     private const val MINOR = 4
-    private const val PATCH = 1
+    private const val PATCH = 2
 
     const val CODE = MAJOR * 10000 + MINOR * 100 + PATCH
     const val NAME = "$MAJOR.$MINOR.$PATCH"
